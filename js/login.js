f387be5293fb73eeb3a4aa2f981073a82ace1fd2
Daniel Villamil <daniel.villamil@peoplesvoice.co>
2018-06-28 21:22:39 -0500

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+/****
+Se encarga de invocar a la funcion validateForm() una vez el usuario hace clic en iniciar sesion
+****/
+function onSubmit() {
+  validateForm();
+}
+
+function validateForm(){
+  console.log("llego");
+  
+}
